701f55142ea508ab80cc00b62dcd77f70556ce56
--global <k@mail.com>
2024-05-06 14:32:19 +0300

    klo 14,tausta liikkuu

---
+++ Assets/Scripts/Space.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Space : MonoBehaviour
+{
+    [Range(0.1f, 2.0f)]               //luo sliderin editoriin annetuilla min/max -arvoilla
+    [SerializeField] float scrollSpeed; //SerializeField pit‰‰ muuttujan privatena,
+                                        //mutta antaa asettaa arvon editorissa
+    Renderer rend;
+    float offset;
+
+    private void Awake()
+    {
+        rend = GetComponent<Renderer>(); //haetaan referenssi vain kerran muistiin
+    }
+
+    void Update() //kutsutaan joka framella
+    {
+        offset = Time.time * scrollSpeed; //kulunut aika * valittu nopeus
+        rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+    }
+}
